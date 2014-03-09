708b6dd1b2d178f8657e878ec0700743a3c6cb1f
jakewilson801 <jacob.grant.wilson.801@gmail.com>
2014-03-09 14:51:56 -0600

    updates

--- app.js
+++ app.js
@@ -9,9 +9,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/test');
 app.configure(function() {
 	app.use(express.static(__dirname + '/public')); 		
 	app.use(express.logger('dev')); 						
-	app.use(express.bodyParser()); 
-	//app.use(express.json());
-	//app.use(express.urlencoded());							
+	app.use(express.bodyParser()); 							
 	app.use(express.methodOverride()); 						
 
 });
@@ -40,17 +38,18 @@ var Selfie = mongoose.model('Selfie', {
 		numYes: Number, 
 		numNo: Number,
 		notASelfie: Number,
-		parentData: Object
+		parentData: Object,
+		batchUrl: String
 });
 
-app.post('/selfie/vote', function(req, res){
-	
+app.post('/selfie/vote', function(req, res){	
 	Selfie.create({
 		url : req.body.url,
 		numYes : req.body.numYes,
 		numNo : req.body.numNo,
 		notASelfie : req.body.notASelfie,
-		parentData : req.body.parentData
+		parentData : req.body.parentData,
+		batchUrl : req.body.batchUrl
 	}, function ( err, selfie){
 		if(err)
 			res.send(err); 
@@ -64,3 +63,6 @@ app.get('*', function(req, res) {
 
 app.listen(3000);
 console.log('Listening on port 3000');
+
+
+
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -8,9 +8,7 @@ haterade.controller('SelfieController', function ($scope, $http) {
 
     fetchData($http, $scope, 'https://api.instagram.com/v1/tags/selfie/media/recent?client_id=339b727c83104de1aecd91778ab4daae');
 
-  $scope.voteOnSelfie = function(selfie, action, count) {
-  		$scope.selfie.url = selfie.images.standard_resolution.url;
-
+  	$scope.voteOnSelfie = function(selfie, action, count) {
   		switch(action){
   			case 0:
   				$scope.selfie.notASelfie = 1; 
@@ -28,30 +26,25 @@ haterade.controller('SelfieController', function ($scope, $http) {
   				$scope.selfie.numNo = 0; 
   			break; 
   		}
+
+  		$scope.selfie.url = selfie.images.standard_resolution.url;
   		$scope.selfie.parentData = selfie; 
-  		console.log($scope.count); 
+  		$scope.selfie.batchUrl = $scope.nextUrl; 
 		$http.post('/selfie/vote', $scope.selfie)
 			.success(function(data) {
 				console.log(data);
 			})
 			.error(function(data) {
 				console.log('Error: ' + data);
-			});
-
+		});
 		
 		$scope.count++; 
-		 
-
 		if(($scope.count - 1) == 19){
 			console.log($scope.nextUrl)
 			fetchData($http, $scope, $scope.nextUrl); 
 			$scope.count = 0; 
-		}
-
-		
+		}		
 	};
- 
-
 });
 
 function fetchData(http, scope, url){
