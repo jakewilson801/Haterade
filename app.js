91ef6c8002eb21031ea43887bbf00a4512d85980
jakewilson801 <jacob.grant.wilson.801@gmail.com>
2014-03-08 22:36:31 -0700

    biscuits

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
 var express = require('express');
 var app = express();
 var request = require('request');
-var apiUrl = ''; 
-//var mongoose = require('mongoose'); 		
+var mongoose = require('mongoose'); 		
 //
 //connect to local mongodb database
-//mongoose.connect('mongodb://127.0.0.1:27017/test');
+mongoose.connect('mongodb://127.0.0.1:27017/test');
 
 app.configure(function() {
 	app.use(express.static(__dirname + '/public')); 		
 	app.use(express.logger('dev')); 						
-	//app.use(express.bodyParser()); 
-	app.use(express.json());
-	app.use(express.urlencoded());							
+	app.use(express.bodyParser()); 
+	//app.use(express.json());
+	//app.use(express.urlencoded());							
 	app.use(express.methodOverride()); 						
 
 });
 
 //attach lister to connected event
-// mongoose.connection.once('connected', function() {
-// 	console.log("Connected to database")
-// });
+mongoose.connection.once('connected', function() {
+	console.log("Connected to database")
+});
 
 app.post('/selfie', function(req, res){
 	
@@ -29,42 +28,40 @@ app.post('/selfie', function(req, res){
 	    res.send(body)
 	  }else{
 	  	console.log(error);
+	  	res.send(error); 
 	  }
 
 	});
 	console.log('fetched pics');
 });
 
-// var Selfie = mongoose.model('Selfie', {
-// 		url : String,
-// 		numYes: Number, 
-// 		numNo: Number,
-// 		notASelfie: Number,
-// 		parentData: Object
-// });
+var Selfie = mongoose.model('Selfie', {
+		url : String,
+		numYes: Number, 
+		numNo: Number,
+		notASelfie: Number,
+		parentData: Object
+});
 
 app.post('/selfie/vote', function(req, res){
 	
-	// Selfie.create({
-	// 	url : req.body.url,
-	// 	numYes : req.body.numYes,
-	// 	numNo : req.body.numNo,
-	// 	notASelfie : req.body.notASelfie,
-	// 	parentData : req.body.parentData
-	// }, function ( err, selfie){
-	// 	if(err)
-	// 		console.log(err); 
-	// 	console.log('VOTED'); 
-	// }); 
+	Selfie.create({
+		url : req.body.url,
+		numYes : req.body.numYes,
+		numNo : req.body.numNo,
+		notASelfie : req.body.notASelfie,
+		parentData : req.body.parentData
+	}, function ( err, selfie){
+		if(err)
+			res.send(err); 
+		res.send("biscuits"); 
+	}); 
+	res.send(err);
 }); 
 
 app.get('*', function(req, res) {
 		res.sendfile('./public/index.html'); 
 });
-
-
-
-
 
 app.listen(3000);
 console.log('Listening on port 3000');
